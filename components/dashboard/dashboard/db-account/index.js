2f3b35b44549e79abf4564f0e7f82103e4407e06
khoavn02 <khoavungoc02@gmail.com>
2024-02-10 16:15:10 +0700

    fix dashboard missing messages

--- components/dashboard/dashboard/db-account/index.js
+++ components/dashboard/dashboard/db-account/index.js
@@ -2,9 +2,10 @@ import Sidebar from "../common/Sidebar";
 import Header from "@/components/header/dashboard-header";
 import SettingsTabs from "./components/index";
 import Footer from "../common/Footer";
+import { useTranslations } from "next-intl";
 
 const index = () => {
-
+  const t = useTranslations();
   return (
     <>
       {/* End Page Title */}
@@ -26,7 +27,7 @@ const index = () => {
             <div className="row y-gap-20 justify-between items-end pb-60 lg:pb-40 md:pb-32">
               <div className="col-12">
                 <h1 className="text-30 lh-14 fw-600">
-                  {messages?.Dashboard?.Sidebar?.account}
+                  {t("Dashboard.Sidebar.account")}
                 </h1>
               </div>
               {/* End .col-12 */}
