5d3c825d4ec21a6aa2cf002ea0f3a687d720330c
Vu Khoa <130125733+khoavn02@users.noreply.github.com>
2024-02-16 15:19:44 +0700

    Update/cache expired behavior (#48)
    * refactor useHotelList
    
    * add force search

--- hooks/useHotelList.jsx
+++ hooks/useHotelList.jsx
@@ -8,10 +8,38 @@ import { useCallback, useEffect, useState } from "react";
 
 const useHotelList = () => {
   const { getToken } = useAuth();
-  const searchInput = useSearchStore((state) => state.searchInput);
+  const { searchInput, isExpired, setIsExpired } = useSearchStore();
   const isSearchFormVaild = SearchInputSchema.safeParse(searchInput);
   const [loading, setLoading] = useState(true);
   const [hotels, setHotels] = useState([]);
+
+  const fetchHotel = async (sessionId, token) => {
+    if (!sessionId) throw Error("sessionId is required");
+    const url = `${TOURCOMPARE_BE_URL}/api/v1/hotels/session?sessionId=${sessionId}`;
+
+    const data = await axios
+      .get(url, {
+        withCredentials: true,
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => res.data);
+    setIsExpired(data.isExpired);
+    const hotels = data.results;
+    setHotels((prevHotels) => {
+      const updatedHotels = [...prevHotels];
+      for (const newHotel of hotels) {
+        if (
+          !updatedHotels.find(
+            (oldHotel) => oldHotel.travelorLink === newHotel.travelorLink
+          )
+        ) {
+          updatedHotels.push(newHotel);
+        }
+      }
+      return updatedHotels;
+    });
+    return data;
+  };
 
   /**
    * Send command to CrawlerUI
@@ -23,38 +51,14 @@ const useHotelList = () => {
     try {
       const params = new URLSearchParams(window.location.search);
       const sessionId = params.get("sessionId");
-      const fetching = async () => {
-        const data = await axios
-          .get(
-            `${TOURCOMPARE_BE_URL}/api/v1/hotels/session?sessionId=${sessionId}`,
-            {
-              withCredentials: true,
-              headers: { Authorization: `Bearer ${token}` },
-            }
-          )
-          .then((res) => res.data);
-        const hotels = data.results;
-        setHotels((prevHotels) => {
-          const updatedHotels = [...prevHotels];
-          for (const newHotel of hotels) {
-            if (
-              !updatedHotels.find(
-                (oldHotel) => oldHotel.travelorLink === newHotel.travelorLink
-              )
-            ) {
-              updatedHotels.push(newHotel);
-            }
-          }
-          return updatedHotels;
-        });
-        return data;
-      };
-
-      let response = await fetching();
+      let response = await fetchHotel(sessionId, token);
       do {
-        if (response.status === "FINISHED") break;
+        if (response.status === "FINISHED") {
+          response = await fetchHotel(sessionId, token);
+          break;
+        }
         await sleep(2000);
-        response = await fetching();
+        response = await fetchHotel(sessionId, token);
       } while (response.status === "RUNNING");
       setLoading(false);
     } catch (error) {
@@ -66,12 +70,13 @@ const useHotelList = () => {
   }, [isSearchFormVaild.success]);
 
   useEffect(() => {
-    if (isSearchFormVaild.success) fetchHotelList();  
+    if (isSearchFormVaild.success) fetchHotelList();
   }, [isSearchFormVaild.success]);
 
   return {
+    isExpired,
     loading,
-    hotels
+    hotels,
   };
 };
 
