81b997ab8a6700393fb2c080027d0bbfdec85247
khoavn02 <khoavungoc02@gmail.com>
2024-02-17 15:27:33 +0700

    redirect when banned

--- app/[locale]/(others)/sso-callback/page.jsx
+++ app/[locale]/(others)/sso-callback/page.jsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 export default async function SSOCallbackPage() {
   const user = await currentUser();
+  
   if (user) return redirect("/");
   return (
     <>
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,12 +1,44 @@
 import Wrapper from "@/components/layout/Wrapper";
+import { TOURCOMPARE_BE_URL } from "@/constants/environment";
+import { auth, currentUser } from "@clerk/nextjs";
 import MainHome from "./(homes)/home_1/page";
+import { redirect } from "next/navigation";
+import { sleep } from "@/utils/sleep";
 
 export const metadata = {
   title: "GoTrip: Home",
   description: "GoTrip - Travel & Tour",
 };
 
-export default function Home() {
+const UserStatus = {
+  BANNED: "BANNED",
+  DELETED: "DELETED",
+};
+
+export default async function Home() {
+  const user = await currentUser();
+  const publicMetadata = user?.publicMetadata;
+
+  if (
+    (user && publicMetadata?.status === UserStatus.BANNED) ||
+    publicMetadata?.status === UserStatus.DELETED
+  ) {
+    const { getToken } = auth();
+    const token = await getToken();
+
+    const res = await fetch(`${TOURCOMPARE_BE_URL}/api/v1/auth/force-logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({}),
+    });
+    if (res.ok) {
+      redirect("/404");
+    }
+  }
+
   return (
     <>
       <Wrapper>
