d71c74b83a4389f1317cf94bb1a6ee70d92858f4
khoavn02 <khoavungoc02@gmail.com>
2024-02-26 02:53:50 +0700

    update footter

--- components/footer/default/ContactInfo.jsx
+++ components/footer/default/ContactInfo.jsx
@@ -2,31 +2,61 @@ const ContactInfo = () => {
   const contactContent = [
     {
       id: 1,
-      title: "Need live support?",
-      action: "mailto:xyz@abc.com",
-      text: "hi@gotrip.com",
+      title: 'Need live support?',
+      action: 'mailto:xyz@abc.com',
+      text: 'hi@gotrip.com',
     },
     {
       id: 2,
-      title: "Contact Page",
-      action: "/contact",
-      text: "Contact us",
+      title: 'Contact Page',
+      action: [
+        {
+          link: '/contact',
+        },
+      ],
+      text: 'Contact us',
     },
     {
       id: 3,
-      title: "Support WhatsApp Group",
-      action: "/contact",
-      text: "Join our WhatsApp Group",
+      title: 'Support WhatsApp Group',
+      action: [
+        {
+          link: 'https://chat.whatsapp.com/JrGL7UOxEqQIsMhCKivh1S',
+          text: 'Hebrew WhatsApp Group',
+        },
+        {
+          link: 'https://chat.whatsapp.com/JqYHRcwAwQx9bi7b11vg9I',
+          text: 'English WhatsApp Group',
+        },
+      ],
     },
   ];
   return (
     <>
-      {contactContent.map((item) => (
+      {contactContent.map(item => (
         <div className="mt-30" key={item.id}>
-          <div className={"text-14 mt-30"}>{item.title}</div>
-          <a href={item.action} target="_blank" className="text-18 fw-500 text-blue-1 mt-5">
-            {item.text}
-          </a>
+          <div className={'text-16 mt-30'}>{item.title}</div>
+          {Array.isArray(item.action) && (
+            <div>
+              {item.action.map((action, index) => (
+                <a
+                  href={action.link}
+                  key={index}
+                  className="d-block text-18 fw-500 text-blue-1 mt-5"
+                >
+                  {action.text}
+                </a>
+              ))}
+            </div>
+          )}
+          {!Array.isArray(item.action) && (
+            <a
+              href={item.action}
+              className="text-14 text-blue-500 hover:underline"
+            >
+              {item.text}
+            </a>
+          )}
         </div>
       ))}
     </>
