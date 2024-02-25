c5ec5aefc8b1681da66f5596e11375005700f030
khoavn02 <khoavungoc02@gmail.com>
2024-02-26 02:22:47 +0700

    add icon search

---
+++ components/hotel-list/hotel-list-v5/HotelNameFilter.jsx
@@ -0,0 +1,42 @@
+import useHotelFilterStore from '@/store/useHotelFilterStore';
+import useHotelNameFilterStore from '@/store/useHotelNameFilterStore';
+import { useEffect, useState } from 'react';
+
+export default function HotelNameFilter({ disabled }) {
+  const { hotels, setHotels } = useHotelNameFilterStore();
+  const { filterHotels, setFilterHotels } = useHotelFilterStore();
+  const [hotelName, setHotelName] = useState('');
+
+  const onInputChange = e => {
+    const val = e.target.value;
+    setHotelName(val);
+  };
+
+  useEffect(() => {
+    if (hotelName) {
+      const filteredHotels = hotels.filter(hotel => {
+        return hotel.title.toLowerCase().includes(hotelName.toLowerCase());
+      });
+      setFilterHotels(filteredHotels);
+    } else {
+      setFilterHotels(hotels);
+    }
+  }, [hotelName]);
+
+  useEffect(() => {
+    if (!disabled) setHotels(filterHotels);
+  }, [disabled]);
+
+  return (
+    <div className="d-flex items-center border rounded-pill w-100">
+      <input
+        className="lh-1 px-10 py-5"
+        placeholder="Hotel name"
+        value={hotelName || ''}
+        disabled={disabled}
+        onChange={onInputChange}
+      />
+        <div className="icon-search text-20 mr-10"></div>
+    </div>
+  );
+}
