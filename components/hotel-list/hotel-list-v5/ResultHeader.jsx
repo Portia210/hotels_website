00d6a955c54e873546a911a560d64d3285bc7323
khoavn02 <khoavungoc02@gmail.com>
2024-02-26 14:24:07 +0700

    update filter trans

--- components/hotel-list/hotel-list-v5/ResultHeader.jsx
+++ components/hotel-list/hotel-list-v5/ResultHeader.jsx
@@ -1,25 +1,33 @@
+import useTrans from '@/hooks/useTrans';
+
 export default function ResultHeader({ loading, isExpired, totalResult }) {
+  const { t, isReverse } = useTrans();
   if (isExpired) {
     return <h3>Results are not updated, please search again</h3>;
+  }
+
+  const renderHotelFound = () => {
+    const text = t('ResultHeader.hotelsFound');
+    return text.replace('x', totalResult);
   }
   return (
     <>
       {loading && (
         <div className="d-flex align-items-center x-gap-10">
           <div className="spinner-border spinner-border-sm" role="status">
-            <span className="visually-hidden">Loading...</span>
+            <span className="visually-hidden">{t('ResultHeader.loading')}...</span>
           </div>
           {totalResult > 0 ? (
-            <h3>{totalResult} Hotels found</h3>
+            <h3>{renderHotelFound()}</h3>
           ) : (
-            <h3>Loading</h3>
+            <h3 dir={isReverse ? 'rtl' : 'ltr'}>{t('ResultHeader.loading')}...</h3>
           )}
         </div>
       )}
       {!loading && totalResult === 0 && !isExpired && <h3>No hotels found</h3>}
       {!loading && totalResult > 0 && (
         <h3 className="text-24 fw-600 text-dark-1">
-          {totalResult} Hotels found
+          {renderHotelFound()}
         </h3>
       )}
     </>
