cba8ceb49ae1132a68d462f4c2c19cd01c99df58
Vu Khoa <130125733+khoavn02@users.noreply.github.com>
2024-02-26 15:07:57 +0700

    update footer trans (#70)

--- components/footer/default/FooterContent.jsx
+++ components/footer/default/FooterContent.jsx
@@ -1,17 +1,47 @@
-import Link from "next/link";
-import footerDataContent from "../../../data/footerContent";
+import Link from 'next/link';
 
-const FooterContent = () => {
+const FooterContent = ({ t }) => {
+  const data = [
+    {
+      id: 1,
+      title: t('Footer.quickLinks'),
+      menuList: [
+        { name: t('Footer.userGuide'), routerPath: '/' },
+        { name: t('Footer.termOfUse'), routerPath: '/' },
+        { name: t('Footer.privacyPolicy'), routerPath: '/' },
+      ],
+    },
+    {
+      id: 2,
+      title: t('Footer.services'),
+      menuList: [
+        { name: t('Footer.shortenLink'), routerPath: '/shorten-link' },
+        {
+          name: t('Footer.citiesInCountry'),
+          routerPath: '/#destinationsWeLove',
+        },
+        {
+          name: t('Footer.popularTouristAttractions'),
+          routerPath: '/#destinationsWeLove',
+        },
+      ],
+    },
+  ];
+
   return (
     <>
-      {footerDataContent.map((item) => (
+      {data.map(item => (
         <div className="col-xl-4 col-lg-4 col-sm-6" key={item.id}>
           <h5 className="text-16 fw-500 mb-30">{item.title}</h5>
           <div className="d-flex y-gap-10 flex-column">
             {item.menuList.map((menu, i) => (
-              <Link href={menu.routerPath} key={i}>
-                {menu.name}
-              </Link>
+              <Link
+                href={menu.routerPath}
+                key={i}
+                dangerouslySetInnerHTML={{
+                  __html: <>{menu.name}</>
+                }}
+              ></Link>
             ))}
           </div>
         </div>
--- components/footer/default/index.jsx
+++ components/footer/default/index.jsx
@@ -1,21 +1,23 @@
-import AppButton from "./AppButton";
-import ContactInfo from "./ContactInfo";
-import Copyright from "./Copyright";
-import FooterContent from "./FooterContent";
+import { useTranslations } from 'next-intl';
+import ContactInfo from './ContactInfo';
+import Copyright from './Copyright';
+import FooterContent from './FooterContent';
+import useTransServer from '@/hooks/useTransServer';
 
 const index = () => {
+  const { t, isReverse } = useTransServer();
   return (
     <footer className="footer -type-1">
-      <div className="container">
+      <div className="container" dir={isReverse ? 'rtl' : 'ltr'}>
         <div className="pt-60 pb-60">
           <div className="row y-gap-40 justify-between xl:justify-start">
             <div className="col-xl-4 col-lg-4 col-sm-6">
-              <h5 className="text-16 fw-500 mb-30">Contact</h5>
-              <ContactInfo />
+              <h5 className="text-16 fw-500 mb-30">{t('Footer.contact')}</h5>
+              <ContactInfo t={t} />
             </div>
             {/* End col */}
 
-            <FooterContent />
+            <FooterContent t={t} />
             {/* End footer menu content */}
           </div>
         </div>
