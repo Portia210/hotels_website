6ab668a8e136ecb2cd8c5ee4b770871a66705163
khoavn02 <khoavungoc02@gmail.com>
2024-03-06 15:59:42 +0700

    add filter for both

--- hooks/hotelFilters/useRatingFilter.jsx
+++ hooks/hotelFilters/useRatingFilter.jsx
@@ -1,20 +1,33 @@
 import { FILTER_TYPE } from '@/hooks/hotelFilters';
-import { useState } from 'react';
+import eventEmitter from '@/utils/eventEmitter';
+import { useState, useEffect } from 'react';
 import { defaultFilter } from '.';
 
-const useRatingFilter = (hotelFilterStore) => {
+export const ratingFilterChangeEvent = 'ratingFilterChange';
+
+const useRatingFilter = hotelFilterStore => {
   const [ratingFilter, setRatingFilter] = useState(defaultFilter.ratingFilter);
   const { setCondition } = hotelFilterStore;
 
   const handleRatingFilterChange = value => {
-    setRatingFilter(prev => {
-      if (prev === value) {
-        return 0;
-      }
-      return value;
-    });
-    setCondition(FILTER_TYPE.RATING, value);
+    eventEmitter.emit(ratingFilterChangeEvent, value);
   };
+
+  const subscribe = () => {
+    const token = eventEmitter.addListener(ratingFilterChangeEvent, value => {
+      setRatingFilter(prev => {
+        if (prev === value) return 0;
+        return value;
+      });
+      setCondition(FILTER_TYPE.RATING, value);
+    });
+    return token;
+  };
+
+  useEffect(() => {
+    const token = subscribe();
+    return () => token.remove();
+  }, []);
 
   return {
     ratingFilter,
