fd1dcf7c643d78f5aaeb2b449231f23b969c974f
khoavn02 <khoavungoc02@gmail.com>
2024-03-07 00:34:00 +0700

    update footer

--- components/footer/default/FooterContent.jsx
+++ components/footer/default/FooterContent.jsx
@@ -1,15 +1,26 @@
 import Link from 'next/link';
 
 const FooterContent = ({ t, isReverse }) => {
-  
   const data = [
     {
       id: 1,
       title: t('Footer.quickLinks'),
       menuList: [
-        { name: t('Footer.userGuide'), routerPath: '/' },
-        { name: t('Footer.termOfUse'), routerPath: '/' },
-        { name: t('Footer.privacyPolicy'), routerPath: '/' },
+        {
+          name: t('Footer.userGuide'),
+          routerPath: 'https://docs.agent-space.com/user-guide',
+          target: '_blank',
+        },
+        {
+          name: t('Footer.termOfUse'),
+          routerPath: 'https://docs.agent-space.com/terms-of-use',
+          target: '_blank',
+        },
+        {
+          name: t('Footer.privacyPolicy'),
+          routerPath: 'https://docs.agent-space.com/privacy-policy',
+          target: '_blank',
+        },
       ],
     },
     {
@@ -33,12 +44,15 @@ const FooterContent = ({ t, isReverse }) => {
     <>
       {data.map(item => (
         <div className="col-xl-3 col-lg-3 col-sm-6" key={item.id}>
-          <h5 className={`text-16 mb-30 fw-${isReverse ? '600': '500'}`}>{item.title}</h5>
+          <h5 className={`text-16 mb-30 fw-${isReverse ? '600' : '500'}`}>
+            {item.title}
+          </h5>
           <div className="d-flex y-gap-10 flex-column">
             {item.menuList.map((menu, i) => (
               <Link
                 href={menu.routerPath}
                 key={i}
+                target={menu?.target || ''}
                 dangerouslySetInnerHTML={{
                   __html: <>{menu.name}</>,
                 }}
