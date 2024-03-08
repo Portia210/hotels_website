c77ebd143decc85552b1706f0041b4e5aa165504
khoavn02 <khoavungoc02@gmail.com>
2024-03-08 15:18:51 +0700

    update lang structure

--- i18n.js
+++ i18n.js
@@ -1,5 +1,12 @@
-import { getRequestConfig } from "next-intl/server";
+import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default,
-}));
+export default getRequestConfig(async ({ locale }) => {
+  const messages = {
+    ...(await import(`./messages/${locale}.json`)).default,
+    ...(await import(`./messages/${locale}/dashboard.json`)).default,
+    ...(await import(`./messages/${locale}/hotel.json`)).default,
+    ...(await import(`./messages/${locale}/pricing.json`)).default,
+    ...(await import(`./messages/${locale}/filterbar.json`)).default,
+  };
+  return { messages };
+});
