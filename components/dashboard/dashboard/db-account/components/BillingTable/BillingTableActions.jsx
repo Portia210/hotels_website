9396605e7e6f47ad48e79c0cab37b7b55b3b1d80
khoavn02 <khoavungoc02@gmail.com>
2024-03-16 23:03:29 +0700

    view invoice

--- components/dashboard/dashboard/db-account/components/BillingTable/BillingTableActions.jsx
+++ components/dashboard/dashboard/db-account/components/BillingTable/BillingTableActions.jsx
@@ -1,6 +1,35 @@
+import { TRANZILA_INVOICE_URL } from '@/constants/invoice';
+import useInvoice from '@/hooks/useInvoice';
+import { useMutation } from '@tanstack/react-query';
+
 export default function BillingTableActions({ row }) {
+  const { getInvoice } = useInvoice();
+
+  const viewInvoiceMutation = useMutation({
+    mutationFn: () => {
+      return getInvoice(row?.original?._id);
+    },
+    onSuccess: data => {
+      if (data.retrieval_key)
+        window.open(`${TRANZILA_INVOICE_URL}/${data.retrieval_key}`, '_blank');
+    },
+  });
+
+  const onViewInvoice = () => {
+    viewInvoiceMutation.mutate();
+  };
+
+  if (viewInvoiceMutation.isPending) {
+    return (
+      <div className="text-center">
+        <span className="btn badge rounded-pill text-bg-primary">
+          Loading...
+        </span>
+      </div>
+    );
+  }
   return (
-    <div className="text-center">
+    <div className="text-center" onClick={() => onViewInvoice()}>
       <span className="btn badge rounded-pill text-bg-primary">Invoice</span>
     </div>
   );
---
+++ constants/invoice.js
@@ -0,0 +1,2 @@
+export const TRANZILA_INVOICE_URL =
+  'https://my.tranzila.com/api/get_financial_document';
