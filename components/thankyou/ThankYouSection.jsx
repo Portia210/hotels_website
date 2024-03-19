ba02e91464f2e14529d131829a465c1401ac04aa
khoavn02 <khoavungoc02@gmail.com>
2024-03-19 14:36:53 +0700

    add spacing

--- components/thankyou/ThankYouSection.jsx
+++ components/thankyou/ThankYouSection.jsx
@@ -8,7 +8,7 @@ export default function ThankYouSection() {
   const { t } = useTrans();
 
   return (
-    <div className="bg-white vh-100 d-flex justify-content-center align-items-center mt-3 mb-3 ms-2 me-2">
+    <div className="bg-white vh-100 d-flex justify-content-center align-items-center mb-3 ms-2 me-2">
       <div className="container " style={{ maxWidth: 600 }}>
         <div className="mb-4 text-center">
           <svg
@@ -22,12 +22,12 @@ export default function ThankYouSection() {
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
           </svg>
         </div>
-        <div className="text-center">
+        <div className="text-center y-gap-10">
           <h2 className="mb-3">{t('ThankYou.thank_you_purchase')}</h2>
           <p className="fs-5">{t('ThankYou.invoice_sent_email')}</p>
           <p className="fs-5">{t('ThankYou.view_payment_details')}</p>
           <p className="fs-5">{t('ThankYou.need_explanation_user_guide')}</p>
-          <div className="container text-center">
+          <div className="container text-center mt-10">
             <div className="row g-0">
               <div className="col-12 col-sm-4">
                 <button
