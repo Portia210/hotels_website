84f8a447c63ebca379e0a8ff7c990153d2144fe6
khoavn02 <khoavungoc02@gmail.com>
2024-03-22 23:17:07 +0700

    update code

--- app/[locale]/(landing)/landing/page.jsx
+++ app/[locale]/(landing)/landing/page.jsx
@@ -1,4 +1,5 @@
 import Landing from '@/components/landing';
+import Head from 'next/head';
 
 export const metadata = {
   title: 'Agent-Space: Landing Page',
@@ -6,5 +7,15 @@ export const metadata = {
 };
 
 export default function LandingPage() {
-  return <Landing />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+        />
+      </Head>
+      <Landing />
+    </>
+  );
 }
