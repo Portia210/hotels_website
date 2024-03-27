0741c2c33ab37545b5bcc7d25dfdf336f35bc933
khoavn02 <khoavungoc02@gmail.com>
2024-03-27 15:15:44 +0700

    refetch when result expired

--- components/hotel-list/hotel-list-v5/MainFilterSearchBox.jsx
+++ components/hotel-list/hotel-list-v5/MainFilterSearchBox.jsx
@@ -1,48 +1,47 @@
-"use client";
+'use client';
 
-import GuestSearch from "@/components/hero/hero-1/GuestSearch";
-import LocationSearch from "@/components/hero/hero-1/LocationSearch";
-import useSearchBar from "@/hooks/useSearchBar";
-import DateSearch from "../common/DateSearch/DateSearch";
-import useTransStore from "@/store/useTransStore";
+import GuestSearch from '@/components/hero/hero-1/GuestSearch';
+import LocationSearch from '@/components/hero/hero-1/LocationSearch';
+import useSearchBar from '@/hooks/useSearchBar';
+import DateSearch from '../common/DateSearch/DateSearch';
+import useTransStore from '@/store/useTransStore';
 
 const MainFilterSearchBox = () => {
   const { handleSearch } = useSearchBar();
-  const messages = useTransStore((state) => state.messages);
+  const messages = useTransStore(state => state.messages);
 
   return (
-    <>
-      <div className="mainSearch -col-3-big bg-white px-10 py-10 lg:px-20 lg:pt-5 lg:pb-20 rounded-4 mt-30">
-        <div className="button-grid items-center">
-          <LocationSearch messages={messages?.SearchBox} />
-          {/* End Location */}
+    <div className="mainSearch -col-3-big bg-white px-10 py-10 lg:px-20 lg:pt-5 lg:pb-20 rounded-4 mt-30">
+      <div className="button-grid items-center">
+        <LocationSearch messages={messages?.SearchBox} />
+        {/* End Location */}
 
-          <div className="searchMenu-date px-30 lg:py-20  sm:px-20 js-form-dd js-calendar">
-            <div>
-              <h4 className="text-15 fw-500 ls-2 lh-16">
-                {messages?.SearchBox?.checkin} - {messages?.SearchBox?.checkout}
-              </h4>
-              <DateSearch />
-            </div>
+        <div className="searchMenu-date px-30 lg:py-20  sm:px-20 js-form-dd js-calendar">
+          <div>
+            <h4 className="text-15 fw-500 ls-2 lh-16">
+              {messages?.SearchBox?.checkin} - {messages?.SearchBox?.checkout}
+            </h4>
+            <DateSearch />
           </div>
-          {/* End check-in-out */}
-
-          <GuestSearch />
-          {/* End guest */}
-
-          <div className="button-item h-full">
-            <button
-              onClick={() => handleSearch("/hotel-list")}
-              className="button -dark-1 py-15 px-40 h-full col-12 rounded-0 bg-blue-1 text-white"
-            >
-              <i className="icon-search text-20 mr-10" />
-              {messages?.SearchBox?.search}
-            </button>
-          </div>
-          {/* End search button_item */}
         </div>
+        {/* End check-in-out */}
+
+        <GuestSearch />
+        {/* End guest */}
+
+        <div className="button-item h-full">
+          <button
+            id="hotelSearchBtn"
+            onClick={() => handleSearch('/hotel-list')}
+            className="button -dark-1 py-15 px-40 h-full col-12 rounded-0 bg-blue-1 text-white"
+          >
+            <i className="icon-search text-20 mr-10" />
+            {messages?.SearchBox?.search}
+          </button>
+        </div>
+        {/* End search button_item */}
       </div>
-    </>
+    </div>
   );
 };
 
--- components/hotel-list/hotel-list-v5/ResultHeader.jsx
+++ components/hotel-list/hotel-list-v5/ResultHeader.jsx
@@ -1,34 +1,49 @@
 import useTrans from '@/hooks/useTrans';
+import { useEffect } from 'react';
 
 export default function ResultHeader({ loading, isExpired, totalResult }) {
   const { t, isReverse } = useTrans();
+
+  const refetch = () => {
+    if (!isExpired) return;
+    setTimeout(() => {
+      document?.getElementById('hotelSearchBtn')?.click();
+    }, 3000);
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [isExpired]);
+
   if (isExpired) {
-    return <h3>Results are not updated, please search again</h3>;
+    return <h3>Results are not updated, searching again...</h3>;
   }
 
   const renderHotelFound = () => {
     const text = t('ResultHeader.hotelsFound');
     return text.replace('x', totalResult);
-  }
+  };
   return (
     <>
       {loading && (
         <div className="d-flex align-items-center x-gap-10">
           <div className="spinner-border spinner-border-sm" role="status">
-            <span className="visually-hidden">{t('ResultHeader.loading')}...</span>
+            <span className="visually-hidden">
+              {t('ResultHeader.loading')}...
+            </span>
           </div>
           {totalResult > 0 ? (
             <h3>{renderHotelFound()}</h3>
           ) : (
-            <h3 dir={isReverse ? 'rtl' : 'ltr'}>{t('ResultHeader.loading')}...</h3>
+            <h3 dir={isReverse ? 'rtl' : 'ltr'}>
+              {t('ResultHeader.loading')}...
+            </h3>
           )}
         </div>
       )}
       {!loading && totalResult === 0 && !isExpired && <h3>No hotels found</h3>}
       {!loading && totalResult > 0 && (
-        <h3 className="text-24 fw-600 text-dark-1">
-          {renderHotelFound()}
-        </h3>
+        <h3 className="text-24 fw-600 text-dark-1">{renderHotelFound()}</h3>
       )}
     </>
   );
