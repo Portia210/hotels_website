0d35020f308d9b1c3ef824dd238ac8d59d37018e
khoavn02 <khoavungoc02@gmail.com>
2024-03-27 17:44:41 +0700

    add trans

--- app/[locale]/(landing)/landing/page.jsx
+++ app/[locale]/(landing)/landing/page.jsx
@@ -1,4 +1,5 @@
 import Landing from '@/components/landing';
+import useTransServer from '@/hooks/useTransServer';
 
 export const metadata = {
   title: 'Agent-Space: Landing Page',
@@ -6,5 +7,11 @@ export const metadata = {
 };
 
 export default function LandingPage() {
-  return <Landing />;
+  const { isReverse } = useTransServer();
+  
+  return (
+    <div dir={`${isReverse && 'rtl'}`}>
+      <Landing />;
+    </div>
+  );
 }
