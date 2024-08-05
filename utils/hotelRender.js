65bf6abbc4d26c003378fc09b0b7d3c95ac2fb52
khoavn02 <khoavungoc02@gmail.com>
2024-08-05 20:08:37 +0700

    update layout

---
+++ utils/hotelRender.js
@@ -0,0 +1,19 @@
+export const renderText = (t, key, value) => {
+  if (key === 'distance' && value) {
+    if (value.includes('from center')) {
+      value = value.replace('from center', t(`Hotel.${key}`));
+    } else if (value.includes('from map center')) {
+      value = value.replace('from map center', t(`Hotel.${key}`));
+    }
+  }
+  return value;
+};
+
+export const toTravelorWithoutSession = url => {
+  if (!url) return;
+  const searchParams = new URLSearchParams(new URL(url).search);
+  searchParams.delete('session');
+  const updatedUrl = new URL(url);
+  updatedUrl.search = searchParams.toString();
+  window.open(updatedUrl.toString(), '_blank').focus();
+};
